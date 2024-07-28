f57c01f6a48981c522a47c8e0d2a5322592a01b3
ea-analisisdatos <erikaalvares.analisisdatos@gmail.com>
2024-07-28 13:44:09 +0200

    Estructuración de la carpeta del proyecto para Streamlit

--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-# tests/conftest.py
-import sys
-import os
-
-# Agregar el directorio src a la ruta de importación
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
